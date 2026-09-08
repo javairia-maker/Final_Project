850aaa9889d57bd5ff612b499fa5f3366ce7ffba
Atiqah <xtmrmoon7@gmail.com>
2026-09-08 05:57:45 -0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,71 @@
+<?php
+
+include("dp.php");
+
+if(isset($_POST['register']))
+{
+
+$name=$_POST['name'];
+
+$email=$_POST['email'];
+
+$password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+$sql="INSERT INTO users(name,email,password)
+
+VALUES('$name','$email','$password')";
+
+if(mysqli_query($conn,$sql))
+{
+
+echo "Registration Successful";
+
+}
+else
+{
+
+echo "Registration Failed";
+
+}
+
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Register</title>
+
+</head>
+
+<body>
+
+<form method="POST">
+
+<input type="text" name="name" placeholder="Name" required>
+
+<br><br>
+
+<input type="email" name="email" placeholder="Email" required>
+
+<br><br>
+
+<input type="password" name="password" placeholder="Password" required>
+
+<br><br>
+
+<button type="submit" name="register">
+
+Register
+
+</button>
+
+</form>
+
+</body>
+
+</html>
